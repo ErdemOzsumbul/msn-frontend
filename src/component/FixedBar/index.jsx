90ddc75862ab450a3c22d9f28b1965759bc5281c
ErdemOzsumbul <e.ozsumbul@gmail.com>
2024-06-08 18:07:06 +0300

    .

--- src/component/FixedBar/index.jsx
+++ src/component/FixedBar/index.jsx
@@ -5,8 +5,13 @@ import { ImHeadphones } from "react-icons/im";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import { useParams } from "react-router-dom/dist";
 import { Bounce, toast } from "react-toastify";
+import { useState } from "react";
+import { MdOutlineFavorite } from "react-icons/md";
+import { MdFavoriteBorder } from "react-icons/md";
 
 function FixedBar() {
+  const [status, setStatus] = useState(null);
+  const [fav, setFav] = useState(false);
   const params = useParams();
 
   const handleCopy = (id) => {
@@ -40,6 +45,26 @@ function FixedBar() {
       >
         <div
           style={{
+            background: fav ? "green" : "#fff",
+            height: "3rem",
+            width: "3rem",
+            display: "flex",
+            gap: "1rem",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: "0.5rem",
+            cursor: "pointer",
+          }}
+          onClick={() => setFav((prv) => !prv)}
+        >
+          {fav ? (
+            <MdOutlineFavorite size={21} color="#fff" />
+          ) : (
+            <MdFavoriteBorder size={21} />
+          )}
+        </div>
+        <div
+          style={{
             background: "#fff",
             height: "3rem",
             width: "3rem",
@@ -55,7 +80,8 @@ function FixedBar() {
         </div>
         <div
           style={{
-            background: "#fff",
+            background: status == "dislike" ? "green" : "#fff",
+            color: status == "dislike" ? "#fff" : "#000",
             height: "3rem",
             width: "3rem",
             display: "flex",
@@ -64,13 +90,18 @@ function FixedBar() {
             alignItems: "center",
             cursor: "pointer",
             borderRadius: "0.5rem",
+            transition: "ease-in-out 0.3s",
           }}
+          onClick={() =>
+            setStatus((prv) => (prv == "dislike" ? null : "dislike"))
+          }
         >
           <BiSolidLike size={23} />
         </div>
         <div
           style={{
-            background: "#fff",
+            background: status == "like" ? "red" : "#fff",
+            color: status == "like" ? "#fff" : "",
             height: "3rem",
             width: "3rem",
             display: "flex",
@@ -79,7 +110,9 @@ function FixedBar() {
             alignItems: "center",
             cursor: "pointer",
             borderRadius: "0.5rem",
+            transition: "ease-in-out 0.3s",
           }}
+          onClick={() => setStatus((prv) => (prv == "like" ? null : "like"))}
         >
           <BiSolidDislike size={23} />
         </div>
